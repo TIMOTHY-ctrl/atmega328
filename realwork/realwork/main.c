8f7c1b71faedb4c1d59a1952f289fed315a58910
TIMOTHY-ctrl <mwizerwatimothy204@gmail.com>
2025-10-22 08:13:24 +0300

    project code

---
+++ realwork/realwork/main.c
@@ -0,0 +1,18 @@
+/*
+ * realwork.c
+ *
+ * Created: 22 Oct 2025 07:50:10
+ * Author : CODEZ
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
